f67a0c90b7e1e8455626326d5823bba9ef9d9ed4
MRX <94168923+sappola@users.noreply.github.com>
2023-01-26 21:58:04 +0530

    Update Logo2.py

--- Telegram/modules/Logo2.py
+++ Telegram/modules/Logo2.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [@SUPERIOR_BOTS](https://t.me/SUPERIOR_BOTS)",
+            caption="Logo by [@Livs_robot](https://t.me/SUPERIOR_BOTS)",
             force_document=False,
         )
         os.remove(flnme)
